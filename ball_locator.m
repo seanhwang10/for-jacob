a8f107067084b95ce214f806dcf9852b8cf30ca4
Sean Hwang <hwang103@purdue.edu>
2020-11-28 04:32:35 -0500

    Update ball_locator.m

--- ball_locator.m
+++ ball_locator.m
@@ -20,25 +20,20 @@ for c = 1:6
     end 
 end 
 
-% for i = 1:11 
-%     plot(pos_raw(i,2), pos_raw(i,3),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
-%     hold on 
-%     plot(pos_raw(i,4), pos_raw(i,5),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
-%     hold on 
-%     plot(pos_raw(i,6), pos_raw(i,7),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
-%     hold off 
-%     axis([0 200 0 200]);
-%     pause(1)
-% end 
+%Ball positioning 
 
 
-% for i=1:n
-%     plot(XY(1,i),XY(2,i),'or','MarkerSize',5,'MarkerFaceColor','r')
-%     hold on 
-%     plot(XY(1,i),XY(2,i),'or','MarkerSize',2,'MarkerFaceColor','b')
-%     hold off 
-%     legend('original','new')
-%     axis([-5 5 -5 5])
-%     pause(1)
-% end
+
+%Displaying player & ball position by time 
+for i = 1:data_count*5  
+    plot(pos_extp(i,1), pos_extp(i,2),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
+    hold on 
+    plot(pos_extp(i,3), pos_extp(i,4),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
+    hold on 
+    plot(pos_extp(i,5), pos_extp(i,6),'or', 'MarkerSize',5,'MarkerFaceColor','r') 
+    hold off 
+    legend('Player 1','Player 2', 'Player 3')
+    axis([0 200 0 200]);
+    pause(0.3)
+end 
 
